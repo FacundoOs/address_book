b6d4745e3a71fcb813cb90275607002c60ba76d6
Facundo Osores <facundo@MBPdeFacundo2.lan>
2020-08-17 15:09:33 +0200

    new styles added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,35 +1,41 @@
-const  storage = window.localStorage
+const storage = window.localStorage
 
-const  renderContacts = () => {
-  const  contacts = JSON.parse(storage.getItem('contacts'))
+const renderContacts = () => {
+	const contacts = JSON.parse(storage.getItem('contacts'))
 
-  let  div = document.querySelector('#contact-list')
-  if (contacts) {
-	div.innerHTML = ''
-	const  ul = document.createElement('ul')
+	let div = document.querySelector('#contact-list')
+	if (contacts) {
+		div.innerHTML = ''
+		const ul = document.createElement('ul')
 
-	contacts.forEach(contact  => {
-		let  li = document.createElement('li')
+		contacts.forEach(contact => {
+			let li = document.createElement('li')
 
-		li.innerHTML = `
-		  <span>${contact.name}</span> |
-		  <span>${contact.email}</span> |
-		  <span>${contact.phone}</span>
-	    `
-	    ul.appendChild(li)
-	  })
-			
-	  div.appendChild(ul)
+			if (contact.name || contact.email || contact.phone || contact.company
+				|| contact.notes || contact.twitter) {
+				li.innerHTML = `
+		  		<span>${contact.name}</span> |
+		 		<span>${contact.email}</span> |
+		 		<span>${contact.phone}</span> |
+				<span>${contact.company}</span> |
+				<span>${contact.notes}</span> |
+		  		<span>${contact.twitter}</span> |
+	  			`
+				ul.appendChild(li)
+			}
+		})
+
+		div.appendChild(ul)
 	} else {
-	  div.innerHTML = '<p>You have no contacts in your address book</p>'
+		div.innerHTML = '<p>You have no contacts in your address book</p>'
 	}
 }
 document.addEventListener('DOMContentLoaded', () => {
 	renderContacts()
-    const  contactForm = document.getElementById('new-contact-form')
-    const  toggleFormVisibilityButton = document.getElementById('add-contact')
+	const contactForm = document.getElementById('new-contact-form')
+	const toggleFormVisibilityButton = document.getElementById('add-contact')
 	contactForm.style.display = 'none'
- 
+
 	toggleFormVisibilityButton.addEventListener('click', () => {
 		if (contactForm.style.display === '') {
 			contactForm.style.display = 'none'
@@ -37,24 +43,24 @@ document.addEventListener('DOMContentLoaded', () => {
 			contactForm.style.display = ''
 		}
 	})
-	contactForm.addEventListener('submit', event  => {
+	contactForm.addEventListener('submit', event => {
 		event.preventDefault()
 
 		// 1. Read all the input fields and get their values
 		const { name, email, phone, company, notes, twitter } = contactForm.elements
 
-		const  contact = {
-			name:  name.value,
-			email:  email.value,
-			phone:  phone.value,
-			company:  company.value,
-			notes:  notes.value,
-			twitter:  twitter.value,
+		const contact = {
+			name: name.value,
+			email: email.value,
+			phone: phone.value,
+			company: company.value,
+			notes: notes.value,
+			twitter: twitter.value,
 		}
 
 		console.log(contact)
 
-		let  contacts = JSON.parse(storage.getItem('contacts')) || []
+		let contacts = JSON.parse(storage.getItem('contacts')) || []
 
 		contacts.push(contact)
 
@@ -62,5 +68,5 @@ document.addEventListener('DOMContentLoaded', () => {
 		storage.setItem('contacts', JSON.stringify(contacts))
 		renderContacts()
 		contactForm.reset()
-   })
+	})
 })
